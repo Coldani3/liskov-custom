97eaddc053595e1c65838052dc0997f9e56b451a
Coldani3 <p110117069@gmail.com>
2020-11-05 09:28:46 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace liskov_custom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
