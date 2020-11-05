332a4317f5a9a4990ea370f5b551fb19119a9a3d
Coldani3 <p110117069@gmail.com>
2020-11-05 09:39:14 +0000

    Stuff

---
+++ Animal.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace liskovcustom
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Born");
+        }
+
+        public void MakeNoise()
+        {
+            Console.WriteLine("breathe");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-namespace liskov_custom
+namespace liskovcustom
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public void DoTheThing(Animal animal)
+        {
+            animal.MakeNoise();
         }
     }
 }
